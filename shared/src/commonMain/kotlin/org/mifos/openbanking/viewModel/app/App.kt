137f78e1bf932462fbb5f930e4dd2e37966afda3
Lakhwinder Singh Sarao <lssarao411@gmail.com>
2023-07-03 02:15:35 +0200

    Update App.kt

--- shared/src/commonMain/kotlin/org/mifos/openbanking/viewModel/app/App.kt
+++ shared/src/commonMain/kotlin/org/mifos/openbanking/viewModel/app/App.kt
@@ -3,43 +3,38 @@ package org.mifos.openbanking.viewModel.app
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import org.mifos.openbanking.ApplicationDispatcher
 import org.mifos.openbanking.base.Response
 import org.mifos.openbanking.coroutines.launchSilent
+import org.mifos.openbanking.data.datasources.disk.DiskDataSource
 import org.mifos.openbanking.domain.usecase.fetchBanks.FetchBanksRequest
 import org.mifos.openbanking.domain.usecase.fetchBanks.FetchBanksResponse
 import org.mifos.openbanking.domain.usecase.fetchBanks.FetchBanksUseCase
-import kotlin.coroutines.CoroutineContext
 
 object App {
 
-    // LIVE DATA
-    val supportedBanksLiveData = MutableStateFlow<SupportedBanksState>(
-        LoadingSupportedBanksState
-    )
-
-    // USE-CASES
+    val supportedBanksLiveData = MutableStateFlow<SupportedBanksState>(LoadingSupportedBanksState)
     private val fetchBanksUseCase = FetchBanksUseCase()
 
-    // ASYNC - COROUTINES
-    // private val coroutineContext by KodeinInjector.instance<CoroutineContext>()
+    private val coroutineContext = ApplicationDispatcher
     private val job: Job = Job()
     private val exceptionHandler = CoroutineExceptionHandler { _, _ -> }
 
-    // private val diskDataSource by KodeinInjector.instance<DiskDataSource>()
+    private val diskDataSource = DiskDataSource()
 
     fun appLaunch() {
-        // fetchBanks()
+        fetchBanks()
     }
-//
-//    private fun fetchBanks() = launchSilent(coroutineContext, exceptionHandler, job) {
-//        val response = fetchBanksUseCase.execute(FetchBanksRequest())
-//        if (response is Response.Success<FetchBanksResponse>) {
-//            val bankList = response.data.bankList
-//            val sortedBy = bankList.sortedBy { it.shortName }
-//            // diskDataSource.saveSupportedBanks(sortedBy)
-//            supportedBanksLiveData.value = SuccessSupportedBanksState
-//        } else if (response is Response.Error) {
-//            supportedBanksLiveData.value = ErrorSupportedBanksState(response.message)
-//        }
-//    }
+
+    private fun fetchBanks() = launchSilent(coroutineContext, exceptionHandler, job) {
+        val response = fetchBanksUseCase.execute(FetchBanksRequest())
+        if (response is Response.Success<FetchBanksResponse>) {
+            val bankList = response.data.bankList
+            val sortedBy = bankList.sortedBy { it.shortName }
+            diskDataSource.saveSupportedBanks(sortedBy)
+            supportedBanksLiveData.value = SuccessSupportedBanksState
+        } else if (response is Response.Error) {
+            supportedBanksLiveData.value = ErrorSupportedBanksState(response.message)
+        }
+    }
 }
